8dd6e81c982cc8e078590bcbe0998bb1ae67d9f6
Tian Wang <twang95@stanford.edu>
2015-12-10 00:35:40 -0800

    Start coreference

--- Code/preprocessCoref.py
+++ Code/preprocessCoref.py
@@ -3,29 +3,34 @@ import xml.etree.ElementTree
 from DataClasses import *
 
 def main():
+    os.system("rm -rf storyTexts")
+    os.system("mkdir storyTexts")
+    if not os.path.exists("./storyTexts/filelist"):
+        open("./storyTexts/filelist", 'w').close()
     for dataset in ["mc160.dev", "mc160.train", "mc160.test", "mc500.dev", "mc500.train", "mc500.train", "mc500.test"]:
         extractStoryTextsToFolder(dataset)
         # run coref here
-        parseXML()
+        #parseXML()
 
 
 
 def extractStoryTextsToFolder(dataset):
     data = Dataset()
     data.appendData("../MCTest/" + dataset)
-    os.system("rm -rf storyTexts")
-    os.system("mkdir storyTexts")
-    with open("./storyTexts/filelist", "w") as filelist:
+    with open("./storyTexts/filelist", "a") as filelist:
         for story in data.stories:
             print story.storyID
+            filePrefix = "../../CS221_FinalProject/Code/storyTexts/"
             storyText = story.casedText
-            filePath =  story.storyID
+            filePath =  filePrefix + story.storyID
             filelist.write(filePath)
             filelist.write("\n")
             with open("./storyTexts/" + story.storyID, "w") as f:
                 f.write(story.casedText)
                 f.flush()
                 os.fsync(f.fileno())    
+            filelist.flush()
+            os.fsync(filelist.fileno())
 
 
 def parseXML():
